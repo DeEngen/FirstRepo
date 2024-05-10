0bf7293e0fa11fea7175aeedb8b0bc5571adb506
DeEngen <tshedisosefako10@gmail.com>
2024-05-10 14:30:29 +0200

    First Change

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
         System.out.println("Hello world!");
+        String name = input.next();
+        System.out.println("Welcome" + name);
     }
 }
